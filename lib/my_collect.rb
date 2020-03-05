a5383a07fc278fbdad19d9189a0f4249f5f8a02a
C. Leow <chienleow1@gmail.com>
2020-03-05 01:29:22 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
+def my_collect(array)
+  i = 0
+  array1 = []
+  while i < array.length
+    array1 << yield(array[i])
+  # my_collect(languages) do |language|
+  # language.upcase
+  # end
+end
 
